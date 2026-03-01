1640a835186f0c0318385b155d4d4beaa9e3456a
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-03-01 12:22:01 -0300

    inicio projeto

---
+++ viewer/templates/app.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto dentista</title>
+</head>
+<body>
+
+    <div>
+        <header>
+            <div>
+                <h1>Logo</h1>
+                <p>token</p>
+                <div>
+                    <p>img</p>
+                    <p>Olá, usuário</p>
+                </div>
+            </div>
+        </header>
+        <div></div>
+    </div>
+
+
+
+
+</body>
+</html>
