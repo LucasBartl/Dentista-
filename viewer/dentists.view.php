aadd4da38c5bcd933ff8f77edb1a616514d756ab
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-03-24 16:26:41 -0300

    Ajustando tela de colaboradores

--- viewer/dentists.view.php
+++ viewer/dentists.view.php
@@ -1,33 +1,34 @@
-<!-- Box dados dentistas  -->
-<div class="containerDentists none">
-    <div class="flex  bx justify-content-center align-items-center gap-3">
-        <div class="flex  flex-column  cardDentist  " style="width: 50%; height: 100%;">
-            <div class=" flex  flex-column hidden imgajuste" style="width: 100%; height: 65%;"><img src="./images/jose.png" alt=""></div>
-            <div class=" description text-center hidden " style="width: 100%; height: 35%;">
-                <h3>José Dente</h3>
-                <p>Dentista geral</p>
-                <hr color="blue">
-                <p>10 anos de experiencia</p>
-                <p>Especialista em implantodontia</p>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="boxTodo">
-    <header>
-        <div class="flex headerCollaborators">
-            <div class="flex bxHeader  align-items-center  justify-content-center">Administrativo</div>
-            <div class="flex bxHeader  align-items-center justify-content-center">Terceirizados</div>
-            <div class="flex bxHeader  align-items-center justify-content-center">Dentistas</div>
+<div class="boxTodo ">
+    <header class="">
+        <div class="flex headerCollaborators ">
+            <h6 id="admin" class="flex bxHeader align-items-center  justify-content-center">Administrativo</h6>
+            <h6 class="flex bxHeader  align-items-center justify-content-center">Terceirizados</h6>
+            <h6 class="flex bxHeader  align-items-center justify-content-center">Dentistas</h6>
         </div>
     </header>
 
+    <div id="collaborators" class=" none" style="height: 88%;">
+        <div class="containerColab">
+            <div>
+                <div class="flex  flex-column  cardDentist   ">
+                    <div class=" flex  flex-column hidden "><img src="./images/jose.png" alt=""></div>
+                    <div class=" description text-center hidden aling-itens-center">
+                        <h3>José Dente</h3>
+                    </div>
+                </div>
+            </div>
 
+        </div>
+    </div>
+</div>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 <script>
-$(document).ready(function(){
-    $('#search').mask('000.000.000-00');
-});
+    $(document).ready(function() {
+        $('#admin').click(function() {
+            const collaborators = document.querySelector('#collaborators');
+            collaborators.classList.remove('none');
+        });
+    });
 </script>
